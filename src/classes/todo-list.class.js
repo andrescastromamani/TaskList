8157ae4e78bc82a2d888bf6f64adc4ab4c1755d2
Andres <castroandresrm@gmail.com>
2021-03-11 20:57:54 -0400

    Gurdamos y recuperamos LocalStorage

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -1,16 +1,18 @@
 export class TodoList {
 
     constructor(){
-        this.todos = [];
+        this.cargarLocalStorage();
 
     }
 
     insertarTodo( todo ){
         this.todos.push( todo );
+        this.guardarLocalStorage();
     }
 
     eliminarTodo( id ){
         this.todos = this.todos.filter( todo => todo.id != id);
+        this.guardarLocalStorage();
     }
 
     completadoTodo( id ){
@@ -24,13 +26,14 @@ export class TodoList {
 
     eliminarCompletados(){
         this.todos = this.todos.filter( todo => !todo.completado);
+        this.guardarLocalStorage();
     }
 
     guardarLocalStorage(){
-
+        localStorage.setItem('todo', JSON.stringify(this.todos));
     }
 
     cargarLocalStorage(){
-        
+        this.todos = (localStorage.getItem('todo')) ? JSON.parse(localStorage.getItem('todo')) : [];
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import { Todo , TodoList } from './classes';
 import { crearTodoHtml } from './js/componentes';
 
 export const todoList = new TodoList();
-const tarea = new Todo('Aprender JavaScript');
-todoList.insertarTodo(tarea);
-console.log(todoList);
-crearTodoHtml(tarea);
 
-localStorage.setItem('key','abcd1234');
-sessionStorage.setItem('key1', 'abcssds');
+todoList.todos.forEach(todo => crearTodoHtml( todo ));
 
