c5f1a37aaf6f9ac6b43243845b6fc17b7a9808cb
Andres <castroandresrm@gmail.com>
2021-03-11 20:15:15 -0400

    localStorage y sessionStorage

--- src/index.js
+++ src/index.js
@@ -7,3 +7,7 @@ const tarea = new Todo('Aprender JavaScript');
 todoList.insertarTodo(tarea);
 console.log(todoList);
 crearTodoHtml(tarea);
+
+localStorage.setItem('key','abcd1234');
+sessionStorage.setItem('key1', 'abcssds');
+
