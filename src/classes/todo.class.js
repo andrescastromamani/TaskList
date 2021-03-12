694e492c30c5213c1a6329963f9747c93688e8d3
Andres <castroandresrm@gmail.com>
2021-03-12 17:00:48 -0400

    Reconstruimos las instancias de los Todos

--- src/classes/todo.class.js
+++ src/classes/todo.class.js
@@ -1,8 +1,19 @@
 export class Todo{
+    static fromJson({id, tarea, completado, creado}){
+        const tempTodo = new Todo(tarea);
+         tempTodo.id = id;
+         tempTodo.completado = completado;
+         tempTodo.creado = creado;
+         return tempTodo;
+    }//Recuperar mis metodos de esta clase
     constructor(tarea){
         this.tarea  = tarea;
         this.id = new Date().getTime();
         this.completado = false;
         this.creado = new Date();
+    }
+
+    imprimirClases(){
+        console.log(`${this.tarea} - ${this.id}`)
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { Todo , TodoList } from './classes';
 import { crearTodoHtml } from './js/componentes';
 
 export const todoList = new TodoList();
-
 todoList.todos.forEach(todo => crearTodoHtml( todo ));
 
+const newTodo = new Todo('aprender JS');
+console.log('todos', todoList.todos);
